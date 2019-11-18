7a55b2e60a2246fb68c6c917e9769d633f0d64a3
Thijmen <Thijmenh01@gmail.com>
2019-11-18 12:30:39 +0100

    merge

--- WaterMuseum_Unity/Assets/Content/Thijmen/Scripts/DayNightCycle.cs
+++ WaterMuseum_Unity/Assets/Content/Thijmen/Scripts/DayNightCycle.cs
@@ -7,10 +7,9 @@ public class DayNightCycle : MonoBehaviour {
     [SerializeField] private float currentTime;
 
     void Update() {
-        currentTime += Time.deltaTime / 60 * 4;
+        currentTime += Time.deltaTime / 60 * 4; //Makes 1 day = 6 minutes.
         transform.eulerAngles = Vector3.left * Mathf.Lerp( 0 , 360 , currentTime / 24f);
-
-        if(currentTime >= 24) {
+        if(currentTime > 24) {
             currentTime = 0; 
         }
     }
