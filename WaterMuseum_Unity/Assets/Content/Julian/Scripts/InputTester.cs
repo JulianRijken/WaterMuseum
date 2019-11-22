c54cb65415ff9b7098c557f3aa55f9161a226fd5
Julian <Rijkenjulian@gmail.com>
2019-11-22 21:58:01 +0100

    Removed New Input System

---
+++ WaterMuseum_Unity/Assets/Content/Julian/Scripts/InputTester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTester : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Old Input");
+            }
+        }
+
+    }
+}
