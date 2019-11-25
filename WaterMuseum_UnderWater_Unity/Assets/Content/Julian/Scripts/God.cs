28f47b729482b82e38644535bebf4f98c278dfe4
Julian <Rijkenjulian@gmail.com>
2019-11-25 11:55:29 +0100

    Sand Added

--- WaterMuseum_UnderWater_Unity/Assets/Content/Julian/Scripts/God.cs
+++ WaterMuseum_UnderWater_Unity/Assets/Content/Julian/Scripts/God.cs
@@ -7,19 +7,12 @@ public class God : MonoBehaviour
 {
     [SerializeField] string m_treeName;
     [SerializeField] private Vector2 m_treeHeightOffest;
-    private Tool m_tool;
+
     private Camera m_mainCamera;
 
     private void Awake()
     {
         m_mainCamera = Camera.main;
-        NotificationCenter.FireToolSwitch(Tool.tree);
-        NotificationCenter.OnToolSwitch += HandleToolSwitch;
-    }
-
-    private void OnDestroy()
-    {
-        NotificationCenter.OnToolSwitch -= HandleToolSwitch;
     }
 
     private void Update()
@@ -29,44 +22,21 @@ public class God : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
             if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
-                switch (m_tool)
+            {
+                if (touch.phase == TouchPhase.Began)
                 {
-                    case Tool.tree:
-
-                        if (touch.phase == TouchPhase.Began)
-                        {
-                            Ray ray = m_mainCamera.ScreenPointToRay(touch.position);
-                            RaycastHit hit;
-                            if (Physics.Raycast(ray, out hit))
-                            {
-                                Vector3 spawnPoint = hit.point;
-                                spawnPoint.y += Random.Range(m_treeHeightOffest.x, m_treeHeightOffest.y);
-                                ObjectPooler.SpawnObject(m_treeName, spawnPoint, Quaternion.identity, true);
-                            }
-                        }
-
-                        break;
-                    case Tool.water:
-
-
-                        break;
-                    case Tool.shovel:
-
-
-                        break;
+                    Ray ray = m_mainCamera.ScreenPointToRay(touch.position);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        Vector3 spawnPoint = hit.point;
+                        spawnPoint.y += Random.Range(m_treeHeightOffest.x, m_treeHeightOffest.y);
+                        ObjectPooler.SpawnObject(m_treeName, spawnPoint, Quaternion.identity, true);
+                    }
                 }
+            }
         }
     }
 
-    public void HandleToolSwitch(Tool tool)
-    {
-        m_tool = tool;
-    }
-}
 
-public enum Tool
-{
-    tree = 0,
-    water = 1,
-    shovel = 2
 }
--- WaterMuseum_UnderWater_Unity/Assets/Content/Julian/Scripts/NotificationCenter.cs
+++ WaterMuseum_UnderWater_Unity/Assets/Content/Julian/Scripts/NotificationCenter.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public static class NotificationCenter
 {
-    public static void FireToolSwitch(Tool tool)
-    {
-        OnToolSwitch?.Invoke(tool);
-    }
-    public delegate void ToolSwitchAction(Tool tool);
-    public static event ToolSwitchAction OnToolSwitch;
-
-    public static void FireTimeChange(float time)
-    {
-        OnTimeChange?.Invoke(time);
-    }
-    public delegate void TimeChangeAction(float time);
-    public static event TimeChangeAction OnTimeChange;
+    
 }
