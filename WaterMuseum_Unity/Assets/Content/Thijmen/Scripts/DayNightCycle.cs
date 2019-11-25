060e825729d2d2dedddb543cca3617147b840e10
Julian <Rijkenjulian@gmail.com>
2019-11-25 09:07:24 +0100

    Day And Night Cycle Tweaked

--- WaterMuseum_Unity/Assets/Content/Thijmen/Scripts/DayNightCycle.cs
+++ WaterMuseum_Unity/Assets/Content/Thijmen/Scripts/DayNightCycle.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class DayNightCycle : MonoBehaviour {
 
     [SerializeField] private float currentTime;
+    [SerializeField] private Light sun;
+    [SerializeField] private Light moon;
+    [SerializeField] private AnimationCurve m_dayLightCurve;
+    [SerializeField] private AnimationCurve m_nightLightCurve;
 
     // JULIAN VOOR THIJMEN: ZOG DAT DE ZON INTENSITY LAGER WORD ALS HIJ ONDER GAAT EN OP 0 STAAT ALS HIJ HELEMAAL WEG IS
 
     void Update() {
+
+        sun.intensity = m_dayLightCurve.Evaluate(currentTime / 24f);
+        moon.intensity = m_nightLightCurve.Evaluate(currentTime / 24f);
+
         currentTime += Time.deltaTime / 60 * 4; //Makes 1 day = 6 minutes.
         transform.eulerAngles = Vector3.left * Mathf.Lerp( 0 , 360 , currentTime / 24f);
         if(currentTime > 24) {
