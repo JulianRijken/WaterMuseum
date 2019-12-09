38859b804a0672b77fdb45dd0a404e2aef0ddcad
Thijmen <Thijmenh01@gmail.com>
2019-12-09 14:52:12 +0100

    Fish Spawning

--- WaterMuseum_UnderWater_Unity/Assets/Content/Thijmen/Scripts/AnimalSpawner.cs
+++ WaterMuseum_UnderWater_Unity/Assets/Content/Thijmen/Scripts/AnimalSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimalSpawner : MonoBehaviour {
 
     [SerializeField] private GameObject[] animals;
+    [SerializeField] private Transform[] spawnPoints;
 
     private float spawnCheck = 5f;
 
@@ -15,7 +16,17 @@ public class AnimalSpawner : MonoBehaviour {
     }
 
     private IEnumerator SpawnAnimals(float time) {
+        int rndAnimal;
+        int rndSpawnLoc;
+
         while(true) {
+
+            rndAnimal = Random.Range( 0 , animals.Length);
+            rndSpawnLoc = Random.Range( 0 , spawnPoints.Length);
+
+            if(Stats.GetSheet().m_plasticCount <= 2 && Stats.GetSheet().m_rockCount >= 5) {
+                Instantiate( animals[rndAnimal] , spawnPoints[rndSpawnLoc].position , transform.rotation );
+            }
 
             yield return new WaitForSeconds( time );
         }
