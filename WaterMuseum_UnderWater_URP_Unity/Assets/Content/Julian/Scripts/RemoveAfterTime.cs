1a3a713289eba6998def6371773f9f67018d0c47
Julian Rijken <Rijkenjulian@gmail.com>
2020-01-19 12:05:20 +0100

    Stone Dust added

---
+++ WaterMuseum_UnderWater_URP_Unity/Assets/Content/Julian/Scripts/RemoveAfterTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveAfterTime : MonoBehaviour
+{
+    [SerializeField] private float time;
+
+    void Awake()
+    {
+        Destroy(gameObject,time);
+    }
+
+}
