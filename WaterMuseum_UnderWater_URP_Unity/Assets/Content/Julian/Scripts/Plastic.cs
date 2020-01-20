66fa20c4fb06256defcfd4dc583f2928e4f4b325
Julian Rijken <rijkenjulian@gmail.com>
2020-01-20 10:57:59 +0100

    Bottle Effect Done

--- WaterMuseum_UnderWater_URP_Unity/Assets/Content/Julian/Scripts/Plastic.cs
+++ WaterMuseum_UnderWater_URP_Unity/Assets/Content/Julian/Scripts/Plastic.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Plastic : MonoBehaviour, IRemovable
 {
+
+    [SerializeField] private GameObject effect;
     public void OnRemove()
     {
         Stats.Sheet.m_plasticCount--;
+        Instantiate(effect, transform.position, transform.rotation);
         gameObject.SetActive(false);
     }
 }
